71bc6c87fd4bef62e3fb36ee30dfb3862f87690d
diegobckn <diegobckn@users.noreply.github.com>
2025-07-10 22:52:35 -0300

    agregando entrada/salida de stock

--- src/Pages/Stock.jsx
+++ src/Pages/Stock.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Button, Dialog, Box } from "@mui/material";
 import SideBar from "../Componentes/NavBar/SideBar.jsx";
 import AjusteInventario from "../Componentes/Stock/AjusteInventario.jsx";
+import MovimientoStock from "../Componentes/Stock/MovimientoStock.jsx";
 import StockModel from "../Models/Stock";
 import QrStockMobile from "../Componentes/ScreenDialog/QrStockMobile.jsx";
 import NivelesUnidades from "../Componentes/Stock/NivelesUnidades.jsx";
@@ -10,6 +11,7 @@ import { Typography } from "@mui/joy";
 const Stock = () => {
   // Estado para controlar la apertura/cierre del modal de Ajuste de Inventario
   const [openAjusteInventario, setOpenAjusteInventario] = useState(false);
+  const [verMovStock, setVerMovStock] = useState(false);
   const [openNiveles, setOpenNiveles] = useState(false);
   const [showQrStockMobile, setShowQrStockMobile] = useState(false);
   const [qrLink, setQrLink] = useState("");
@@ -37,7 +39,7 @@ const Stock = () => {
   useEffect(() => {
     loadQrLink()
 
-    setLinkUrl( window.location.href + "mobile" )
+    setLinkUrl(window.location.href + "mobile")
   }, [])
 
   return (
@@ -58,6 +60,17 @@ const Stock = () => {
           onClick={handleOpenAjusteInventario} // Abre el modal al hacer clic en el botón
         >
           Ajuste de inventario
+        </Button>
+
+        <Button
+          variant="outlined"
+          sx={{
+            my: 1,
+            mx: 2,
+          }}
+          onClick={() => { setVerMovStock(true) }} // Abre el modal al hacer clic en el botón
+        >
+          Entrada/Salida
         </Button>
 
         <Button
@@ -91,10 +104,10 @@ const Stock = () => {
 
         {linkUrl != "" && (
           <div style={{
-            padding:"15px",
-            border:"1px solid #ccc",
-            borderRadius:"8px",
-            margin:"10px"
+            padding: "15px",
+            border: "1px solid #ccc",
+            borderRadius: "8px",
+            margin: "10px"
           }}>
             <Typography>Link directo a stock mobile</Typography>
             <Typography><a href={linkUrl}>Hacer click en este boton</a></Typography>
@@ -113,6 +126,15 @@ const Stock = () => {
         fullWidth
       >
         <AjusteInventario onClose={handleCloseAjusteInventario} />
+      </Dialog>
+
+      <Dialog
+        open={verMovStock}
+        onClose={() => { setVerMovStock(false) }}
+        maxWidth="lg"
+        fullWidth
+      >
+        <MovimientoStock onClose={() => { setVerMovStock(false) }} />
       </Dialog>
 
 
