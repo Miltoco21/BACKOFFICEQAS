33f1e4b00cb40285f985aca5b75d7c5f2a032cf6
diegobckn <diegobckn@users.noreply.github.com>
2025-10-10 16:11:27 -0300

    subiendo maxlenght de inputs text

--- src/definitions/Constants.ts
+++ src/definitions/Constants.ts
@@ -1,6 +1,6 @@
 const CONSTANTS = {
     appName: "EasyPOSLite",
-    appVersion: "1.0.258",
+    appVersion: "1.0.259",
     UNIDADES: [
         { idUnidad: 0, descripcion: "Sin unidad" },
         { idUnidad: 1, descripcion: "UNI" },
